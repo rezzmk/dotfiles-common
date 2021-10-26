55ab4082c523764adb740b47e3a320f42dbc111b
Marcos <marcos.caramalho@gmail.com>
2021-10-26 23:15:02 +0100

    Fix symlink script

--- symlink-configs.sh
+++ symlink-configs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Vim stuff
-ln -s vim/vimrc ~/.vimrc
-ln -s vim/ideavimrc ~/.ideavimrc
+ln -s ~/.dotfiles/common/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/common/vim/ideavimrc ~/.ideavimrc
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if ! [ -d "~/.vim/bundle" ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim +PluginInstall +qall
+fi
