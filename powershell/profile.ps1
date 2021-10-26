5a8a8ceac9336b1acf8d388af65c1feeca7426ca
marcos <marcos.caramalho@gmail.com>
2021-10-26 23:00:47 +0100

    Update vim configs

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,10 @@
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module Terminal-Icons
 
-Set-PoshPrompt -Theme Aliens
+#Set-PoshPrompt -Theme mt
+#Set-PoshPrompt -Theme marcduiker
+Set-PoshPrompt -Theme material
 
 cd $([System.Environment]::GetEnvironmentVariable('WORK_MAIN_REPO_DIR','machine'))
 
@@ -16,6 +19,18 @@ function edit-current-posh-theme {
   $returnValue = $(Get-PoshContext)
   vim $returnValue[0]
 }
+function start-docfx-for-cur-dir {
+  docfx --serve docfx.json -p 1243
+}
+function git-pull {
+  git pull
+}
+function get-serial-number {
+	Get-WmiObject win32_bios | select SerialNumber
+}
+
+# Remove builtin aliases
+Remove-Alias gp -Force -ErrorAction SilentlyContinue
 
 Set-Alias -Name reload -Value reload-profile
 Set-Alias -Name gst -Value get-git-status
@@ -23,6 +38,10 @@ Set-Alias -Name config-ini -Value edit-config-ini
 Set-Alias -Name config-winterm -Value edit-win-term-profile
 Set-Alias -Name edit-theme -Value edit-current-posh-theme
 Set-Alias -Name lfg -Value git-lfg
+Set-Alias -Name docfx-start -Value start-docfx-for-cur-dir
+Set-Alias -Name gp -Value git-pull
+Set-Alias -Name serialnumber -Value get-serial-number
+
 
 #$HOST.UI.RawUI.WindowTitle = Get-Location
 
---
+++ symlink-configs.ps1
@@ -0,0 +1,17 @@
+function MkLink {
+	Param (
+		[Parameter(Mandatory=$true, Position=0)] [string] $TargetPath,
+		[Parameter(Mandatory=$true, Position=1)] [string] $SourcePath
+	)
+
+	if(Test-Path -Path $SourcePath) {
+		Write-Output "File already exists, skipping..."
+	}
+	else {
+		Write-Output "`nSymlinking $TargetPath to $SourcePath"
+		New-Item -ItemType SymbolicLink -Path "$SourcePath" -Target "$TargetPath"
+	}
+}
+
+MkLink "vim\ideavimrc" "~\.ideavimrc"
+MkLink "vim\vimrc" "~\.vimrc"
