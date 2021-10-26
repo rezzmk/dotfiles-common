b037af923392095b123eea378ed56b704e45755b
marcos <marcos.caramalho@gmail.com>
2021-10-26 23:08:13 +0100

    Adds symlinking script for linux

--- symlink-configs.ps1
+++ symlink-configs.ps1
@@ -1,11 +1,7 @@
 function MkLink {
-	Param (
-		[Parameter(Mandatory=$true, Position=0)] [string] $TargetPath,
-		[Parameter(Mandatory=$true, Position=1)] [string] $SourcePath
-	)
-
-	if(Test-Path -Path $SourcePath) {
-		Write-Output "File already exists, skipping..."
+	Param ([Parameter(Mandatory=$true, Position=0)] [string] $TargetPath, [Parameter(Mandatory=$true, Position=1)] [string] $SourcePath)
+	if(Test-Path -Path $SourcePath) { 
+		Write-Output "File already exists, skipping..." 
 	}
 	else {
 		Write-Output "`nSymlinking $TargetPath to $SourcePath"
@@ -13,5 +9,5 @@ function MkLink {
 	}
 }
 
-MkLink "vim\ideavimrc" "~\.ideavimrc"
-MkLink "vim\vimrc" "~\.vimrc"
+MkLink "$pwd\vim\ideavimrc" "~\.ideavimrc"
+MkLink "$pwd\vim\vimrc" "~\.vimrc"
---
+++ symlink-configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Vim stuff
+ln -s vim/vimrc ~/.vimrc
+ln -s vim/ideavimrc ~/.ideavimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
